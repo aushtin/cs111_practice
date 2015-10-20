f13c858facfd7461dc9d643f548395622b46e240
aushtin <aust412@gmail.com>
2015-10-19 20:43:10 -0700

    first real commit - lab1a complete

---
+++ cs111_practice/execute-command.c
@@ -0,0 +1,34 @@
+#include "command-internals.h"
+#include "command.h"
+#include "alloc.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int
+command_status (command_t c)
+{
+    return c->status;
+}
+
+
+
+//what is time_travel?
+void
+execute_command (command_t c, int time_travel)
+{
+    /* FIXME: Replace this with your implementation.  You may need to
+     add auxiliary functions and otherwise modify the source code.
+     You can also use external functions defined in the GNU C Library.  */
+    
+    
+    
+    
+}
