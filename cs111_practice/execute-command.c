79b90da5217d2c56ba894a1c24b36fc4fc4edd5f
Daryn Arakawa <daryn.arakawa@gmail.com>
2015-11-09 02:52:07 -0800

    Changed make_read_list for SIMPLE command

--- cs111_practice/execute-command.c
+++ cs111_practice/execute-command.c
@@ -170,11 +170,12 @@ read_list_t make_read_list(read_list_t r_list, command_t c){
         return NULL;
     }
     
-    //if c->output is not NULL, there is a write, add it
+    //if c->input is not NULL, there is a read, add it
     if (c->input){
         rnode_t new_read = create_rnode(c->input);
         add_rnode_to_list(new_read, r_list);
     }
+   
     
     switch (c->type) {
         case AND_COMMAND:
@@ -185,8 +186,16 @@ read_list_t make_read_list(read_list_t r_list, command_t c){
             make_read_list(r_list, c->u.command[1]);
             break;
         }
-        case SIMPLE_COMMAND:
-            break;
+        case SIMPLE_COMMAND:{
+            //check for arguments
+            int i=1;
+            while (c->u.word[i] != NULL){
+                rnode_t new_read = create_rnode(c->u.word[i]);
+                add_rnode_to_list(new_read, r_list);
+                i++;
+            }
+        }
+            
         case SUBSHELL_COMMAND: {
             make_read_list(r_list, c->u.subshell_command);
             break;
@@ -595,7 +604,7 @@ fork_and_begin_exec(command_t command, pid_t *proc_table, int index) {
     else {
         
         proc_table[index] = pid;
-        
+    
     }
     
 }
@@ -608,7 +617,6 @@ void check_dependencies(commandNode_t cNode) {
         
         cNode->dependencies_done = true;
     }
-    
     
     //if there are dependencies, check if theyre done
     else {
@@ -703,7 +711,6 @@ exec_time_travel(command_stream_t cstream) {
             }
         }
         
-        
         //check if any children are DONE
         commandNode_t update;
         update = cstream->head;
@@ -742,7 +749,6 @@ exec_time_travel(command_stream_t cstream) {
     //begin waiting for blocked
     while (number_of_finished != cstream->num_nodes) {
         
-        
         //check if any children are DONE
         commandNode_t update;
         update = cstream->head;
@@ -774,9 +780,7 @@ exec_time_travel(command_stream_t cstream) {
         
         
         if (number_of_finished == cstream->num_nodes) {
-            
             return;
-            
         }
         
         //check dependencies in blocked_commands
